61084cf0356a72b31a8bd2b4af7cecd63b52a022
Leonardo Pimentel Lopes Filho <leo_zinho_filho@hotmail.com>
2023-05-29 08:53:02 -0300

    Day 29-05

---
+++ error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex h-screen justify-center items-center flex-col">
+    <h1 class="text-9xl">404</h1>
+    <p class="mt-7 text-4xl">Page Not Found</p>
+    <button class="rounded mt-7 text-2cl bg-blue-400 px-7 py-4 text-white" @click="voltar()">
+        Go Back
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        voltar(){
+            navigateTo("/")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,39 +1,38 @@
 <script setup>
-  const route = useRoute()
-  useHead({
-    title:toTitleCase(route.params.name)
-  })
+const route = useRoute();
+useHead({
+  title: toTitleCase(route.params.name),
+});
 
-  function toTitleCase(str) {
-  return str.replace(/\w\S*/g, function (txt){
+definePageMeta({
+  layout: "custom",
+});
+
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
-  
 </script>
 <template>
-  <div>
-    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-        <DetailHero />
-        <DetailAttributes />
-        <DetailDescription title="lorem"/>
-        <DetailContact />
-    </div>    
-  </div>
+  <DetailHero />
+  <DetailAttributes />
+  <DetailDescription title="lorem" />
+  <DetailContact />
 </template>
 
 <script>
-import DetailHero from '@/components/Detail/Hero.vue'
-import DetailAttributes from '@/components/Detail/Attributes.vue'
-import DetailDescription from '@/components/Detail/Description.vue'
-import DetailContact from '@/components/Detail/Contact.vue'
+import DetailHero from "@/components/Detail/Hero.vue";
+import DetailAttributes from "@/components/Detail/Attributes.vue";
+import DetailDescription from "@/components/Detail/Description.vue";
+import DetailContact from "@/components/Detail/Contact.vue";
 
 export default {
-    components:{
-        DetailDescription,
-        DetailHero,
-        DetailAttributes,
-        DetailContact
-    }
-}
+  components: {
+    DetailDescription,
+    DetailHero,
+    DetailAttributes,
+    DetailContact,
+  },
+};
 </script>
